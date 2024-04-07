7f047c56f28689b09047e38b67b7dbcf91cccfce
Kirill Lykov <kirillykov07@gmail.com>
2024-04-08 00:54:08 +0300

    classwork:file2

---
+++ OneDrive/Рабочий стол/SEM 04.04.2024/classwork/file02/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает
+// целое число N, а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// Примеры
+// 4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
+// 2 => -2, -1, 0, 1, 2
+
+using Microsoft.Win32.SafeHandles;
+
+Console.WriteLine("input N:");
+int n = Convert.ToInt32(Console.ReadLine());
+int start = -n;
+
+while (start <= n)
+{
+    System.Console.Write(start + " ");
+    start = start + 1;
+}
