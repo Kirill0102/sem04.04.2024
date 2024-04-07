1f3ef4f8248fe1085f04aeb5fb7f6bf74016924d
Kirill Lykov <kirillykov07@gmail.com>
2024-04-08 00:32:18 +0300

    file1:ex 1

--- OneDrive/Рабочий стол/SEM 04.04.2024/classwork/file01/Program.cs
+++ OneDrive/Рабочий стол/SEM 04.04.2024/classwork/file01/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Console.WriteLine("hello");
+// Console.WriteLine("input number:");
+// int a = Convert.ToInt32(Console.ReadLine());
+// // System.Console.WriteLine(a * a);
+// // System.Console.WriteLine("You input -> " + a * a);
+// System.Console.WriteLine($"Quad of {a} -> {a * a}");
+
+
+Console.WriteLine("input first number:");
+int num1 = Convert.ToInt32(Console.ReadLine());//25
+Console.WriteLine("input second number:");
+int num2 = Convert.ToInt32(Console.ReadLine());//5
+
+if (num1 == num2 * num2)
+{
+System.Console.WriteLine($"{num1} is squad of {num2}");
+}
+else
+{
+    System.Console.WriteLine($"{num1} is not squad of {num2}");
+}
