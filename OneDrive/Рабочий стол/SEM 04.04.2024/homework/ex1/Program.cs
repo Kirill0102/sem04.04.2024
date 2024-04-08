a1feb2c3a640f5718864f4afcbb8caa17c319e60
Kirill Lykov <kirillykov07@gmail.com>
2024-04-08 17:09:23 +0300

    создал программу нахождения большего числа из двух

---
+++ OneDrive/Рабочий стол/SEM 04.04.2024/homework/ex1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("input first number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("input second number:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    System.Console.WriteLine("Первое число больше");
+}
+else
+{
+        System.Console.WriteLine("Второе число больше");
+}
