6aaec325cb9246ed660884bccc727c5db6c5bed2
Kirill Lykov <kirillykov07@gmail.com>
2024-04-13 20:18:25 +0300

    add commit massive:ex1;ex2

---
+++ OneDrive/Рабочий стол/GeekBrains/SEM 04.04.2024/massive/ex1/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+while (i < 0)
+{
+    arr[i] = i + 1;
+    // System.Console.WriteLine(arr[i]);
+    // System.Console.WriteLine(' ');
+    System.Console.WriteLine($"{arr[i]} ");
+    i = i + 1;
+}
---
+++ OneDrive/Рабочий стол/GeekBrains/SEM 04.04.2024/massive/ex2/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] array = [1,6,24,12,3,14,7,17,11,10];
+int i = 0;
+
+while(i < n)
+{
+    if(array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
