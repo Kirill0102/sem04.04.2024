505498bef9ed05ba7173b3c742688f07dfa454d0
Kirill Lykov <kirillykov07@gmail.com>
2024-04-14 23:02:29 +0300

    программа покахывающаю в какой четверти будет находиться точка

---
+++ OneDrive/Рабочий стол/GeekBrains/SEM 04.04.2024/homework/homtworkEx2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+
+Console.WriteLine("input X:");
+int X = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input Y:");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+if(X > 0 && Y > 0)
+{
+    System.Console.WriteLine("1 четверть");
+}
+
+if(X > 0 && Y < 0)
+{
+    System.Console.WriteLine("4 четверть");
+}
+
+if(X < 0 && Y > 0)
+{
+    System.Console.WriteLine("2 четверть");
+}
+
+if(X < 0 && Y < 0)
+{
+    System.Console.WriteLine("3 четверть");
+}
