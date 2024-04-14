7b4865be58af34055ade93dd5b432e74194d756f
Kirill Lykov <kirillykov07@gmail.com>
2024-04-14 22:53:43 +0300

    Программа проверяющая число на кратность 7 и 23

---
+++ OneDrive/Рабочий стол/GeekBrains/SEM 04.04.2024/homework/homeworkEx1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+Console.WriteLine("input first number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 % 7 == 0 && num1 % 23 == 0)
+{
+    System.Console.WriteLine(num1);
+}
+else
+{
+    System.Console.WriteLine("Число не кратно 7 и 23 одновременно");
+}
